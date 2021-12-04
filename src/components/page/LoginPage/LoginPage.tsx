bcbac09df18cefe81871321073d84dd89aea263b
Jimmy <nguyenhoangkhang.jim2001@gmail.com>
2021-12-04 21:26:09 +0700

    update loginpage

--- src/components/page/LoginPage/LoginPage.tsx
+++ src/components/page/LoginPage/LoginPage.tsx
@@ -1,11 +1,34 @@
 import React from "react";
 
 import "./style.css";
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import { useNavigate } from "react-router-dom";
 import { useEffect, useRef } from "react";
+import { Checkbox } from "@mui/material";
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 function LoginPage() {
   let navigate = useNavigate();
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
 
   const username = useRef<HTMLInputElement>(null);
   const password = useRef<HTMLInputElement>(null);
@@ -16,6 +39,17 @@ function LoginPage() {
     console.log(password.current?.value);
     // navigate("/", { replace: true });
   };
+  
+  // validate the toggle password
+  const togglePasswordVisibility = () => {
+
+    if(password.current?.type === "password"){
+      password.current.type = "text";
+
+    } else if(password.current?.type === "text" ){
+      password.current.type = "password";
+    }
+  }
 
   return (
     <div>
@@ -53,12 +87,16 @@ function LoginPage() {
                 placeholder="Must be at least 6 characters"
                 maxLength={128}
               />
-              <i
+              {/* <i
                 id="visibility-icon"
                 //   onClick="togglePasswordVisibility()"
                 className="far fa-eye-slash"
-              ></i>
+              ></i> */}
             </div>
+            <span>
+              <input type="checkbox" id="passwordVisibility" onClick={togglePasswordVisibility}  />
+               <span style={{marginLeft: "10px"}}>Show Password</span>
+            </span>
             <p id="password-error" className="error-text"></p>
 
             {/* <div className="input-container cta">
@@ -86,6 +124,29 @@ function LoginPage() {
           </form>
         </div>
       </div>
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
     </div>
   );
 }
