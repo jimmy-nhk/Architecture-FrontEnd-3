406651599f7a983b6fc78dbbc8daabd74b5c6603
thanhlongb <s3748575@rmit.edu.vn>
2021-12-24 19:06:57 +0700

    add tagline field to post create page

--- src/components/post/PostCreatePage/cards/UploadImageCard.tsx
+++ src/components/post/PostCreatePage/cards/UploadImageCard.tsx
@@ -1,20 +1,30 @@
-import { Card, CardContent, Typography, FormControl, Button } from '@mui/material';
+import { Card, CardContent, Typography, FormControl, Button, TextField } from '@mui/material';
 import * as React from 'react';
+import { useState } from 'react';
+
 
 export default function UploadImageCard() {
+  const [postTagline, setPostTagline] = React.useState<string>("")
+
   return (
     <Card>
       <CardContent>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           Cover image
         </Typography>
-        <FormControl>
+        <FormControl fullWidth>
         <label htmlFor="cover-image-upload">
           <input style={{display:"none"}} accept="image/*" id="cover-image-upload" multiple type="file" />
           <Button variant="contained" component="span">
             Upload
           </Button>
         </label>
+        <TextField fullWidth 
+          id="postTagline" 
+          value={postTagline} 
+          onChange={e => setPostTagline(e.target.value)} 
+          label="New post tagline" variant="filled" />
+
         </FormControl>
       </CardContent>
     </Card>
