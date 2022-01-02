8b883821c59449f1b1246aca772b1878de073cb9
s3819660 <56669093+s3819660@users.noreply.github.com>
2022-01-02 23:38:20 +0700

    fixed posts map

--- src/components/post/postContainer/PostContainer.tsx
+++ src/components/post/postContainer/PostContainer.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 import { Box } from "@mui/material";
 import axios, { AxiosResponse } from "axios";
 
-export type ILikedUser = {
+export type LikedUserClass = {
   id: number;
   uid: number;
 };
@@ -15,7 +15,7 @@ export type ILikedUser = {
 //   commentDTO: CommentClass,
 //   userDTO: UserClass
 // }
-export type IPost = {
+export type PostClass = {
   // {id, userId, title, bodyText, category, directors, thumbnailURL, likedCount, tagline, likedUserList, viewCount}
   id: number;
   userId: number | null;
@@ -26,70 +26,48 @@ export type IPost = {
   thumbnailURL: string;
   likedCount: number;
   tagline: string;
-  likedUserList: Array<ILikedUser> | null;
+  likedUserList: Array<LikedUserClass> | null;
   viewCount: number;
 };
 
 function PostContainer() {
-  const [posts, setPosts] = useState<IPost[]>([
-    {
-      id: 2,
-      userId: 2,
-      title: "title 2 ne",
-      bodyText: "<p>cone</p><p>oconte&nbsp;</p><p>ne</p>",
-      category: "Computer Science",
-      directors: "lulu; ",
-      thumbnailURL: "",
-      likedCount: 0,
-      tagline: "tagline 2 ne",
-      likedUserList: [{
-        id: 1,
-        uid: 1
-      }],
-      viewCount: 0
-    }
-  ]);
+  const [posts, setPosts] = useState<PostClass[]>([]);
 
   const getPosts = () => {
     axios
       .get("http://localhost:8085/crud/getPost/pageNo=0&pageSize=20&asc=true")
       .then((response: AxiosResponse) => {
-        // console.log(response.data.content);
-        // var data: IPost[] | null = null;
-        // data = response.data.content as IPost[]
-        var data: IPost[] = [];
-        var post = {
-          id: 0,
-          userId: 0,
-          title: "",
-          bodyText: "",
-          category: "",
-          directors: "",
-          thumbnailURL: "",
-          likedCount: 0,
-          tagline: "",
-          likedUserList: null,
-          viewCount: 0,
-        };
+        // var data: PostClass[] = [];
+        // var post = {
+        //   id: 0,
+        //   userId: 0,
+        //   title: "",
+        //   bodyText: "",
+        //   category: "",
+        //   directors: "",
+        //   thumbnailURL: "",
+        //   likedCount: 0,
+        //   tagline: "",
+        //   likedUserList: null,
+        //   viewCount: 0,
+        // };
         // // {id, userId, title, bodyText, category, directors, thumbnailURL, likedCount, tagline, likedUserList, viewCount}
-        response.data.content.map((x: any) => {
-          console.log(x);
-          post.id = x.id;
-          post.title = x.title;
-          post.userId = x.userId;
-          post.bodyText = x.bodyText;
-          post.category = x.category;
-          post.directors = x.directors;
-          post.thumbnailURL = x.thumbnailURL;
-          post.likedCount = x.likedCount;
-          post.tagline = x.tagline;
-          post.likedUserList = x.likedUserList;
-          post.viewCount = x.viewCount;
-          console.log("post=", post);
-          data.push(post);
-        });
-        // setPosts(data)
-        // setPosts(response.data.content)
+        // response.data.content.map((x: any) => {
+        //   post.id = x.id;
+        //   post.title = x.title;
+        //   post.userId = x.userId;
+        //   post.bodyText = x.bodyText;
+        //   post.category = x.category;
+        //   post.directors = x.directors;
+        //   post.thumbnailURL = x.thumbnailURL;
+        //   post.likedCount = x.likedCount;
+        //   post.tagline = x.tagline;
+        //   post.likedUserList = x.likedUserList;
+        //   post.viewCount = x.viewCount;
+        //   data.push(post);
+        //   console.log("post=", post);
+        // });
+        setPosts(response.data.content)
       });
   };
 
@@ -105,8 +83,8 @@ function PostContainer() {
 
     // setPosts(children);
 
+    getPosts();
     console.log(posts);
-    // getPosts();
   }, []);
 
   useEffect(() => {
@@ -129,14 +107,14 @@ function PostContainer() {
       >
         {!posts
           ? null
-          : posts.map((post, index) => (
-              <Grid item xs={12} md={6}>
+          : posts.map((post) => (
+              <Grid item xs={12} md={6} key={post.id}>
                 <Link
                   className="post-item"
                   to="/post"
                   style={{ textDecoration: "none" }}
                 >
-                  {post}
+                  <Post key={post.id} />
                 </Link>
               </Grid>
             ))}
